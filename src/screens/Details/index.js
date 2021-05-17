1db413d66920b6e31383d8441901d6d354b3a683
Jean Diego <jeandiego.ds@gmail.com>
2021-05-17 20:12:25 -0300

    ✨ Detail screen route

---
+++ src/screens/Details/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import DetailsView from './view';
+
+const Details = () => {
+  return <DetailsView />;
+};
+
+export default Details;
---
+++ src/screens/Details/view.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DetailsView = () => {
+  return (
+    <View>
+      <Text> Tela de Detalhes</Text>
+    </View>
+  );
+};
+
+export default DetailsView;
