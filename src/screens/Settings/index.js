60aa97160d24af457679c37f8f193f8eec61780e
Jean Diego <jeandiego.ds@gmail.com>
2021-05-17 20:11:27 -0300

    ✨ Settings screen route

---
+++ src/screens/Settings/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SettingsView from './view';
+
+const Settings = () => {
+  return <SettingsView />;
+};
+
+export default Settings;
---
+++ src/screens/Settings/view.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const SettingsView = () => {
+  return (
+    <View>
+      <Text>Tela de Configuracoes</Text>
+    </View>
+  );
+};
+
+export default SettingsView;
